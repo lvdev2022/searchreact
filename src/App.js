058e4f01506dd90754d9d528c5c77f74f7d43f3a
Lam <lvdev2022@gmail.com>
2022-09-14 16:49:09 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Users } from './users';
 
 function App() {
+  const [query, setQuery] = useState('');
+
+  //TODO: Add a state variable to hold the filtered users
+
+  //console.log(query);
+  console.log(Users.filter(user => user.first_name.toLowerCase().includes("sol")));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        type="text"
+        placeholder='search'
+        className='search'
+        onChange={e => setQuery(e.target.value)}
+      />
+      <ul className='list'>
+        {Users.filter(user => user.first_name.toLowerCase().includes(query.toLowerCase())).map(user => (
+          <li className='list-items' key={user.id}> {user.first_name}</li>)
+        )}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+
+
+
